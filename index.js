55df7a0e19e9aac518a768b8738d91d70526c3ac
tkogiah <87034470+Tkogiah@users.noreply.github.com>
2021-11-25 02:33:33 -0800

    finished script

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ function log(a) {
 }
 
 const search = document.getElementById('search')
-
+const images = document.getElementById('images')
+const description = document.getElementById('description')
 
 search.addEventListener('keyup', (e) => {
     if(!search.value) return;
+    let searchValue = search.value.toUpperCase()
+    log(searchValue)
     if (e.code === "Enter") { 
-        runSearch(search.value)
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                let test = JSON.parse(xhttp.responseText);
+                let imageArray = test.photos
+                description.innerHTML = `DISPLAYING IMAGES FOR ${searchValue}`
+                for(let i = 0; i < imageArray.length; i++) {
+                    let newImage = document.createElement('img')
+                    newImage.src = imageArray[i].src.medium
+                    newImage.classList.add('photo')
+                    newImage.id = i
+                    images.appendChild(newImage) 
+                }
+            }
+            search.value = ''
+        };
+        xhttp.open("GET", `https://api.pexels.com/v1/search?query=${search.value}`, true);
+        xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001cdefa1219ee747f4959f7436bb7fb6a2')
+        xhttp.send();
     }
 })
-
-function runSearch(param) {
-
-}
-
-function buildDisplay() {
-    
-    let newImageDiv = document.createElement('div')
-}
-
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-};
-xhttp.open("GET", 'https://api.pexels.com/v1/', true);
-xhttp.send();
